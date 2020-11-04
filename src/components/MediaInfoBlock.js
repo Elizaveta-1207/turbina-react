d254f751d5f9219c920211a9381fb7be4df991f2
alex-kurkov <alexkourkov@yandex.ru>
2020-11-04 15:44:03 +0500

    ellipsis trouble solved

--- src/components/MediaInfoBlock.js
+++ src/components/MediaInfoBlock.js
@@ -6,7 +6,7 @@ export default function MediaInfoBlock({ songs, currentSong, contentIsText }) {
     if (songsList.length === 0) return (<p className="infoblock__content infoblock__no-content-msg">Пока это единственный релиз, но скоро будут новые</p>);
     return (
       <ul className="infoblock__songs-container">
-        {songsList.map((song) => (<li className="infoblock__listItem" key={song.url}> <a><span className="infoblock__content infoblock__songItem" href="#">{`${song.title} - ${song.artist}`}</span></a></li>))}
+        {songsList.map((song) => (<li className="infoblock__listItem" key={song.url}> <a className="infoblock__content infoblock__songItem" href="#">{`${song.title} - ${song.artist}`}</a></li>))}
       </ul>
     );
   };
