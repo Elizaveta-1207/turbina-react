6c873725fd18bea877635670c7e0a45925e424bc
alex-kurkov <alexkourkov@yandex.ru>
2020-11-11 20:15:10 +0500

    logo marshak paths&pointer events fixed

--- src/components/CommonUtils/ImageUtils/LogoMarshak.js
+++ src/components/CommonUtils/ImageUtils/LogoMarshak.js
@@ -6,6 +6,8 @@ const PublishingLogoWrap = styled.div`
   width: 50px;
   height: 50px;
   display: block;
+  cursor: inherit;
+  pointer-events: none;
   transition: opacity 0.3s ease-in-out;
   
   &:hover {
@@ -26,15 +28,25 @@ const PublishingLogoWrap = styled.div`
     height: 32px;
   }
 `;
+const ClickablePath = styled.path`
+cursor: pointer;
+`;
+const SvgButton = styled.svg`
+width: 100%;
+height: 100%;
+pointer-events: all;
+`;
 
 const LogoMarshak = ({ fill }) => (
-    <PublishingLogoWrap as="a"
-      href="https://marshakbooks.ru/"
-      rel="turbina"
-      target="_blank">
-      <svg viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 1.98864V26.1364V50H50L48.2249 0H42.3077L40.8284 17.0455H29.2899L28.4024 1.98864L20.1183 1.42045L21.3018 17.0455H8.87574L6.80473 1.42045L0 1.98864Z" fill={fill}/>
-      </svg>
+    <PublishingLogoWrap
+    rel="turbina"
+    target="_blank"
+    as="a"
+    href="https://marshakbooks.ru/">
+      <SvgButton
+        viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <ClickablePath d="M0 1.98864V26.1364V50H50L48.2249 0H42.3077L40.8284 17.0455H29.2899L28.4024 1.98864L20.1183 1.42045L21.3018 17.0455H8.87574L6.80473 1.42045L0 1.98864Z" fill={fill}/>
+      </SvgButton>
     </PublishingLogoWrap>
 );
 
